38ce708d4a187b971f00c5c4e09c304a90d8423a
einez <1.0eine.z@gmail.com>
2017-07-14 13:25:45 +0800

    https://leetcode.com/problems/bitwise-and-of-numbers-range/#/description

--- src/bitManipulation/BitwiseAndOfNumbersRange.java
+++ src/bitManipulation/BitwiseAndOfNumbersRange.java
@@ -5,27 +5,37 @@ package bitManipulation;
  */
 public class BitwiseAndOfNumbersRange {
     public static void main(String[] args) {
-        int result = new BitwiseAndOfNumbersRange().rangeBitwiseAnd(Integer.MAX_VALUE - 1, Integer.MAX_VALUE);
+//        int result = new BitwiseAndOfNumbersRange().rangeBitwiseAnd(Integer.MAX_VALUE - 1, Integer.MAX_VALUE);
+        int result = new BitwiseAndOfNumbersRange().rangeBitwiseAnd(1, 3);
         System.out.println(result);
     }
 
+    //    public int rangeBitwiseAnd(int m, int n) {
+//        int ret = 0;
+//        while (m > 0 && floor(m) == floor(n)) {
+//            int tmp = floor(m);
+//            ret |= tmp;
+//            m -= tmp;
+//            n -= tmp;
+//        }
+//        return ret;
+//    }
+//
+//    public int floor(int m) {
+//        int ret = 1;
+//        while (m > 1) {
+//            m >>= 1;
+//            ret <<= 1;
+//        }
+//        return ret;
+//    }
     public int rangeBitwiseAnd(int m, int n) {
-        int ret = 0;
-        while (m > 0 && floor(m) == floor(n)) {
-            int tmp = floor(m);
-            ret |= tmp;
-            m -= tmp;
-            n -= tmp;
+        int comn = m ^ n;
+        int count = 0;
+        while (comn > 0) {
+            comn >>= 1;
+            count++;
         }
-        return ret;
-    }
-
-    public int floor(int m) {
-        int ret = 1;
-        while (m > 1) {
-            m >>= 1;
-            ret <<= 1;
-        }
-        return ret;
+        return m & n & ~((1 << count) - 1);
     }
 }
